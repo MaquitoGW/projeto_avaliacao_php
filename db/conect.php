b05ebe89c7e2d279bed18d89491d9bd0b2f4d279
MaquitoGw <maicongoncalves826@gmail.com>
2025-01-06 23:32:36 -0300

    New comments

--- db/conect.php
+++ db/conect.php
@@ -1,6 +1,18 @@
 <?php
+/*
+Conexao com Banco de Dados 
+---------
+ATENCAO!
+- Insirar as tabelas ao seu banco de dados, elas estao localizadas em SQL/financeiro.sql
+*/
+
+$dbName = "financeiro";
+$host= "localhost";
+$user = "root";
+$password = "maicon2107";
+
 try {
-    $pdo = new PDO("mysql:dbname=financeiro;host=localhost", "root", "maicon2107");
+    $pdo = new PDO("mysql:dbname=$dbName;host=$host", $user , $password);
 } 
 catch (PDOException $e){
     echo "Erro no banco de dados: ". $e->getMessage();
